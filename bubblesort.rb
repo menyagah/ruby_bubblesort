9d8c2826d7c377e5248e660b70a4fde3e6c52cc3
Hubert MUGABO <mugberto@gmail.com>
2020-06-14 19:57:32 +0200

    Fix format errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,30 +1,30 @@
 def bubble_sort(arr)
-    (arr.length-1).times do |n|
-        for i in (1...arr.length-n)
-            if arr[i-1] > arr[i] 
-                arr[i-1], arr[i] = arr[i], arr[i-1]
-            end
-        end
+  (arr.length - 1).times do |n|
+    for i in (1...arr.length - n)
+      if arr[i - 1] > arr[i]
+        arr[i - 1], arr[i] = arr[i], arr[i - 1]
+      end
     end
-    return arr
+  end
+  arr
 end
 
 def bubble_sort_by(arr)
-    (arr.length-1).times do |n|
-        for i in (1...arr.length-n)
-            orient = yield arr[i-1], arr[i] if block_given?
-            if orient > 0
-                arr[i], arr[i-1] = arr[i-1], arr[i]
-            end
-        end
+  (arr.length - 1).times do |n|
+    for i in (1...arr.length - n)
+      orient = yield arr[i - 1], arr[i] if block_given?
+      if orient > 0
+        arr[i], arr[i - 1] = arr[i - 1], arr[i]
+      end
     end
-    return arr
+  end
+  arr
 end
 
-# bubble_sort_by([3,2,11,34,56,121,131,1,2,11,44,77,88,10001,1000000,0,-1]) do |left, right| 
+# bubble_sort_by([3,2,11,34,56,121,131,1,2,11,44,77,88,10001,1000000,0,-1]) do |left, right|
 #     left - right
 # end
 
-# p bubble_sort_by(["hi","hello","hey"]) do |left, right| 
+# p bubble_sort_by(["hi","hello","hey"]) do |left, right|
 #     left.length - right.length
 # end
