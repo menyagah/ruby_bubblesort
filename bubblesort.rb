8befd9bc3cf848af5f991feb3982c4d2fa1e4c9a
Martin Nyagah <menyagah27@gmail.com>
2020-06-14 21:53:47 +0300

    Remove the return value

--- bubblesort.rb
+++ bubblesort.rb
@@ -13,8 +13,8 @@ def bubble_sort_by(arr)
       if block_given?
         orient = yield arr[i - 1], arr[i]
       else
-        puts "Please add a block!!"
-        return
+        puts 'Please add a block!!'
+        break
       end
       arr[i], arr[i - 1] = arr[i - 1], arr[i] if orient.positive?
     end
@@ -29,3 +29,5 @@ end
 # p bubble_sort_by(["hi","hello","hey"]) do |left, right|
 #     left.length - right.length
 # end
+
+p bubble_sort_by([3,2,11,34,56,121,131,1,2,11,44,77,88,10001,1000000,0,-1]) 
